2e57791370159df476d1d9f461cc66c8acc314cb
qiaofenglin <kkxiaodi@gmail.com>
2022-04-13 00:03:55 +0800

    Fix bug

--- js/main.js
+++ js/main.js
@@ -4,23 +4,57 @@ function random(a,b){
     return Math.round(Math.random()*(a-b)+b);
 }
 
-function move(ele){
-  move(ele,{
+
+function fly(ele,obj,cb){
+    clearInterval(ele.t);
+    ele.t = setInterval(() => {
+        var i = true;
+        for(var attr in obj){
+            var eNow = parseInt(getStyle(ele,attr));
+            let speed = (obj[attr] - eNow)/10;
+            speed = speed < 0 ? Math.floor(speed) : Math.ceil(speed);
+            if(eNow !== obj[attr]){
+                i = false;
+            }
+            ele.style[attr] = eNow + speed + "px";
+        }
+        if(i){
+            clearInterval(ele.t);
+            if(cb){
+                cb();
+            }
+        }
+    }, 30);
+}
+
+function getStyle(ele,attr){
+    if(ele.currentStyle){
+        return ele.currentStyle[attr];
+    }else{
+        return getComputedStyle(ele,false)[attr];
+    }
+}
+
+
+
+
+function Spider(obj){
+  fly(obj,{
     left:random(0,document.documentElement.clientWidth - 20),
     top:random(0,document.documentElement.clientHeight - 20)
     },()=>{
-      move(ele);
+      Spider(obj);
   })
 }
 
 function init(){
-  for(var i=0;i<60;i++){
+  for(var i=0;i<40;i++){
     var div = document.createElement("div");
     div.className = "spider";
     document.body.appendChild(div);
     div.style.left = x + "px";
     div.style.top = y + "px";
-    move(div);
+    Spider(div);
   }
 }
 window.addEventListener('load', init);
