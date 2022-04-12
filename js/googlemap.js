f5aad76f3ac1e386a088c23146d90ff1a44cab39
qiaofenglin <kkxiaodi@gmail.com>
2022-04-12 23:13:46 +0800

    add google remark

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,9 +1,9 @@
 function mapInit(){
   var PathData  = [
-    {lat:30.262319, lng:120.248252},
-    {lat:30.246426, lng:120.235803},
-    {lat:30.224886, lng:120.263543},
-    {lat:30.249505, lng:120.280194},
+    [30.262319, 120.248252],
+    [30.246426, 120.235803],
+    [30.224886, 120.263543],
+    [30.249505, 120.280194],
   ];
   var mapobj = document.getElementById('googleMap');
   var hangzhou = new google.maps.LatLng(30.245560, 120.215357);
@@ -40,31 +40,31 @@ function mapInit(){
 
 
 
-    var path = [];
-    var bounds = new google.maps.LatLngBounds();
-    var center={lat:30.253714, lng:120.260108};
-    console.log(center);
-    bounds.extend(center);
-    for (var i in PathData)
-    {
-        var p = PathData[i];
-        console.log(p);
-        var latlng = new google.maps.LatLng(p[1], p[2]);
-        path.push(latlng);
-        bounds.extend(latlng);
-    }
-    var poly = new google.maps.Polygon({
-        paths: path,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 3,
-        fillColor: '#FF0000',
-        fillOpacity: 0.1
-    });
-    poly.setMap(myMap);
+  var path = [];
+  var bounds = new google.maps.LatLngBounds();
+  var center={lat:30.253714, lng:120.260108};
+  console.log(center);
+  bounds.extend(center);
+  for (var i in PathData)
+  {
+      var p = PathData[i];
+      var latlng = new google.maps.LatLng(p[0], p[1]);
+      console.log(latlng);
+      path.push(latlng);
+      bounds.extend(latlng);
+  }
+  var poly = new google.maps.Polygon({
+      paths: path,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 3,
+      fillColor: '#FF0000',
+      fillOpacity: 0.1
+  });
+  poly.setMap(myMap);
 
 
-    myMap.fitBounds(bounds);
+  myMap.fitBounds(bounds);
 
 }
 
